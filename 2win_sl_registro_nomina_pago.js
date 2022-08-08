21c83d91928bfd6e6e05b14dcb240d8bed9d5ab6
dmunozm <86175879+DimmoX@users.noreply.github.com>
2022-08-08 03:07:52 -0400

    Carga de archivo csv y validación por tipo de archivo.

--- 2win_sl_registro_nomina_pago.js
+++ 2win_sl_registro_nomina_pago.js
@@ -7,18 +7,199 @@
     function(serverWidget) {
 
         function onRequest(context) {
-            if(context.request.method == 'GET'){
+            var bootstrap = '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">';
+            var scriptsBootstrap = `
+                <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+            `
+            var svg = `
+               <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+                   <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+                       <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+                   </symbol>
+                   <symbol id="info-fill" fill="currentColor" viewBox="0 0 16 16">
+                       <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
+                   </symbol>
+                   <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+                       <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                   </symbol>
+               </svg>
+           `
+            
+            var titulo = `<h1>Carga Para Nominas de Pago</h1>`
+            var inputFile = `
+                <form method="POST" action="" enctype="multipart/form-data">
+                    <label class="test" id="label__text" >
+                        <div id="text__file">
+                            Adjuntar Archivo
+                        </div>
+                        <input type="file" class="upload__file" name="file" id="file" onchange="mostrar();">
+                    </label>
+                    <div id="btn__submit">
+                        <input type="submit" value="Cargar">
+                    </div>
+                </form>
+                <script>
+                    var mostrar = () =>{
+                        let btn = document.getElementById("file");
+                        let divText = document.getElementById("text__file");
+                        console.log(btn.value.split('\\\\')[2]);
+                        let textFile = btn.value.split('\\\\')[2];
+                        divText.innerHTML=textFile;
+                        console.log(divText);                                             
+                    }
+                </script>
+            `;
+    
+            var formUploadFile = `
+                    <head>
+                        <style>
+                            *{
+                                margin: 0;
+                                padding: 0;
+                                box-sizing: border-box;
+                            }
+                            .upload__file{
+                                display:none;
+                            }
+                            form{
+                                width: 30%;
+                                display: grid;
+                                grid-template-columns: repeat(3,2fr);
+                                grid-template-areas: 
+                                    "a a b";
+                            }
+                            .container-form{
+                                width: 100%;
+                                height:100vh;
+                            }
+                            .titulo{
+                                margin-left: 10%;
+                                margin-right: 10%;
+                                padding-top: 50px;
+                                font-size: 18px;
+                            }
+                            .form-input{
+                                display: flex;
+                                justify-content: center;
+                                align-items: center;
+                                height: 50%;
+                            }
+                            #text__file, #btn__submit{
+                                border: 1px solid black;
+                                height: 60px;
+                                text-align: center;
+                                display: grid;
+                                align-content: center;
+                                cursor: pointer;
+                            }
+                            #label__text{
+                                grid-area: a;
+                                width: 100%;
+                            }
+                            #text__file{
+                                margin-right: 4px;
+                            }
+                            #btn__submit > input{
+                                background: none;
+                                color: #FFFF;
+                                border: none;
+                                height: inherit;
+                            }
+                            #btn__submit{
+                                grid-area: b;
+                                width: 100%;
+                                color: #FFFF;
+                                background-color: rgb(70, 70, 70);
+                            }
+                            #btn__submit:hover{
+                                background-color: rgb(88, 88, 88);
+                            }
+                            .alert-form{
+                                width: 100%;
+                                display: flex;
+                                justify-content: center;
+                                position: fixed;
+                            }
+                            .alert{
+                                width: 18%;
+                            }
+                            #text__alert{
+                                position: relative;
+                                margin: auto;
+                            }
+                            
+                            
+                        </style>
+                        ${bootstrap}
+                    </head>
+                    <body>
+                        <div class="container-form">
+                            <div class="titulo">
+                                ${titulo}
+                            </div>
+                            <div class="form-input">
+                                ${inputFile}
+                            </div>
+                        </div>
+
+                        ${scriptsBootstrap}
+                    </body>
+                `;
+            if(context.request.method === 'GET'){
                 //TODO carga de archivo csv en netsuite.
-                let field = form.addField({
-                    id: 'textfield',
-                    type: serverWidget.FieldType.TEXT,
-                    label: 'Carga Pago de Nómina'
-                });
+                context.response.write(formUploadFile);
+                
+                
+            } else {
+                //TODO Validación de extensión de archivo,  si es CSV se guardara en el file cabinet, si no, mostrará alerta indicando que se debe adjuntar un archivo de tipo CSV.
+                try{
+                    var fileObj = context.request.files.file;
+                    // log.audit("fileObj", fileObj.fileType.toLowerCase())
+                    var html = ``;
+                    if(fileObj.fileType.toLowerCase() ==='csv'){
+                        fileObj.folder = 3318;
+                        var id = fileObj.save();
+                        html = `
+                            ${formUploadFile}
+                        `
+                        //TODO aplicar redirección hacia otro suitelet desplegado en netsuite (se debe crear), utilizar módulo N/https con su metodo sendRedirect().
+
+                    }else {
+                        var alerta = `
+                            ${svg}
+                            <div class="alert-form" id="alert-form">
+                                <div class="alert alert-danger d-flex align-items-center" role="alert">
+                                    <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+                                    <div id="text__alert">
+                                        El Archivo debe ser de extensión CSV.
+                                    </div>
+                                </div>
+                            </div>
+                            <script>setTimeout();</script>
+                        `
+                        html = `
+                            <script>
+                                setTimeout(function(){
+                                    var mensajeAlerta = document.getElementById("alert-form");
+                                    mensajeAlerta.style.display="none";
+                                }, 5000)
+                            </script>
+                            ${alerta}
+                            ${formUploadFile}
+                            ${scriptsBootstrap}
+                        `
+                    }
+                
+                    context.response.write(html);
+                } catch(error){
+                    log.error("error", error.message);
+                }
             }
+            
         }
 
         return {
             onRequest: onRequest
         };
-     }
-);
+});
