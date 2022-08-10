b9d81a241f1dd83a7be0ef22665036e104ce72e9
dmunozm <86175879+DimmoX@users.noreply.github.com>
2022-08-10 18:44:10 -0400

    add creación json para registrar datos en netsuite

--- libs/2win_lib_procesar_datos_nomina.js
+++ libs/2win_lib_procesar_datos_nomina.js
@@ -3,16 +3,28 @@
  * @module ./2win_lib_search_nominas_de_pago.js
  * @NModuleScope Public
  **/
-define(['N/file'], 
-    function(file) {
+define(['N/file', 'N/record'], 
+    function(file, record) {
         function readPayrollFile(payroll){
             var payrollFile = file.load({id: `SuiteScripts/psinet_registra_pagos_nomina/pago_nominas/${payroll}`});
             var data = [];
+            var json = {};
             payrollFile.lines.iterator().each(function (line) {
                 data = line.value.split(",");
-                log.debug("arreglo data nómina", data);
+                // log.debug("arreglo data nómina", data);
+                for(i in data){
+                    json[i] = data[i];
+                }
+                log.debug("json", json);
+                //TODO crear registro en tabla transacciones, pendiente saber tipo de transacción.
+                // objRecord = record.create({
+                //     type: "string",
+                //     isDynamic: boolean,
+                //     defaultValues: Object
+                // })
                 return true;
             });
+            
         }
         return {
             readPayrollFile : readPayrollFile
