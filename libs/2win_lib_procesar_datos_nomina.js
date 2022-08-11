a781a795b4969c7f199f791cd5493ca63a8c37f8
dmunozm <86175879+DimmoX@users.noreply.github.com>
2022-08-11 17:46:50 -0400

    add creación registro de pago

--- libs/2win_lib_procesar_datos_nomina.js
+++ libs/2win_lib_procesar_datos_nomina.js
@@ -1,10 +1,10 @@
 /**
  * @NApiVersion 2.x
- * @module ./2win_lib_search_nominas_de_pago.js
+ * @module ./2win_lib_procesar_datos_nomina.js
  * @NModuleScope Public
  **/
-define(['N/file', 'N/record'], 
-    function(file, record) {
+define(['N/file', 'N/record', './2win_lib_search_nominas_de_pago.js'], 
+    function(file, record, nominas) {
         function readPayrollFile(payroll){
             var payrollFile = file.load({id: `SuiteScripts/psinet_registra_pagos_nomina/pago_nominas/${payroll}`});
             var data = [];
@@ -15,13 +15,34 @@ define(['N/file', 'N/record'],
                 for(i in data){
                     json[i] = data[i];
                 }
-                log.debug("json", json);
-                //TODO crear registro en tabla transacciones, pendiente saber tipo de transacción.
-                // objRecord = record.create({
-                //     type: "string",
-                //     isDynamic: boolean,
-                //     defaultValues: Object
-                // })
+
+                //TODO crear registro en tabla transacciones de tipo PAGO.
+                
+                objRecord = record.create({
+                    type: record.Type.CUSTOMER_PAYMENT,
+                    isDynamic: true
+                });
+                var name = json[0];
+                log.debug("name", name);
+                var idCustomer = nominas.searchCustomer(name);
+                log.debug("idCustomer", idCustomer);
+                objRecord.setValue({
+                    fieldId: "customer",
+                    value: idCustomer
+                });
+                objRecord.setValue({
+                    fieldId: "subsidiary",
+                    value: 5
+                });
+                objRecord.setValue({
+                    fieldId: "payment",
+                    value: json[3]
+                });
+                var idRecord = objRecord.save({
+                    enableSourcing: false,
+                    ignoreMandatoryFields: false
+                })
+                log.debug("id registro nuevo en trasacciones", idRecord);
                 return true;
             });
             
