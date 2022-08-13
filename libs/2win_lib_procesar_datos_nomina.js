a766b9a2122d9d8a311154b8b1b38fe901101e5e
dmunozm <86175879+DimmoX@users.noreply.github.com>
2022-08-13 17:15:38 -0400

    Se agrega proceso de creación de pago

--- libs/2win_lib_procesar_datos_nomina.js
+++ libs/2win_lib_procesar_datos_nomina.js
@@ -3,12 +3,47 @@
  * @module ./2win_lib_procesar_datos_nomina.js
  * @NModuleScope Public
  **/
-define(['N/file', 'N/record', './2win_lib_search_nominas_de_pago.js'], 
-    function(file, record, nominas) {
+define(['N/file', 'N/record', './2win_lib_search_nominas_de_pago.js', 'N/format'], 
+    function(file, record, nominas, format) {
+
+        function registerPayroll(datosNomina){
+            var objRecord  = record.create({
+                type: "customrecord_2win_regist_nominas_de_pago",
+                isDynamic: true
+            });
+            objRecord.setValue({
+                fieldId: 'custrecord_user',
+                value: datosNomina.user,
+                ignoreFieldChange: true
+            });
+            objRecord.setValue({
+                fieldId: 'custrecord_name_file',
+                value: datosNomina.name_file,
+                ignoreFieldChange: true
+            });
+            objRecord.setValue({
+                fieldId: 'custrecord_date_time',
+                value: datosNomina.date_time,
+                ignoreFieldChange: true
+            });
+            objRecord.setValue({
+                fieldId: 'custrecord_state',
+                value: datosNomina.state,
+                ignoreFieldChange: true
+            });
+            var idRecord = objRecord.save({
+                enableSourcing: false,
+                ignoreMandatoryFields: false
+            });
+
+            return idRecord;
+        }
+
         function readPayrollFile(payroll){
             var payrollFile = file.load({id: `SuiteScripts/psinet_registra_pagos_nomina/pago_nominas/${payroll}`});
             var data = [];
             var json = {};
+            var payments = [];
             payrollFile.lines.iterator().each(function (line) {
                 data = line.value.split(",");
                 // log.debug("arreglo data nómina", data);
@@ -28,27 +63,31 @@ define(['N/file', 'N/record', './2win_lib_search_nominas_de_pago.js'],
                 log.debug("idCustomer", idCustomer);
                 objRecord.setValue({
                     fieldId: "customer",
-                    value: idCustomer
+                    value: idCustomer // 42866
+                });
+                objRecord.setValue({
+                    fieldId: "trandate",
+                    value: format.parse({ value: '12/08/2022', type: format.Type.DATE })
                 });
                 objRecord.setValue({
                     fieldId: "subsidiary",
-                    value: 5
+                    value: 5 // 5 -> pruebas desarrollo || 20 -> Proyectos y Servicios NetSuite
                 });
                 objRecord.setValue({
                     fieldId: "payment",
                     value: json[3]
                 });
-                var idRecord = objRecord.save({
+                var idRecordPago = objRecord.save({
                     enableSourcing: false,
                     ignoreMandatoryFields: false
-                })
-                log.debug("id registro nuevo en trasacciones", idRecord);
-                return true;
+                });
+                payments.push(idRecordPago);
             });
-            
+            return payments;
         }
         return {
-            readPayrollFile : readPayrollFile
+            readPayrollFile : readPayrollFile,
+            registerPayroll : registerPayroll
         };
     
 });
