a43d67037cda07c7841064b838348ec734457dc7
dmunozm <86175879+DimmoX@users.noreply.github.com>
2022-08-17 02:14:36 -0400

    actualización estado, control error registro pagos

--- libs/2win_lib_procesar_datos_nomina.js
+++ libs/2win_lib_procesar_datos_nomina.js
@@ -52,42 +52,64 @@ define(['N/file', 'N/record', './2win_lib_search_nominas_de_pago.js', 'N/format'
                 }
 
                 //TODO crear registro en tabla transacciones de tipo PAGO.
-                
-                objRecord = record.create({
-                    type: record.Type.CUSTOMER_PAYMENT,
-                    isDynamic: true
-                });
-                var name = json[0];
-                log.debug("name", name);
-                var idCustomer = nominas.searchCustomer(name);
-                log.debug("idCustomer", idCustomer);
-                objRecord.setValue({
-                    fieldId: "customer",
-                    value: idCustomer // 42866
-                });
-                objRecord.setValue({
-                    fieldId: "trandate",
-                    value: format.parse({ value: '12/08/2022', type: format.Type.DATE })
-                });
-                objRecord.setValue({
-                    fieldId: "subsidiary",
-                    value: 5 // 5 -> pruebas desarrollo || 20 -> Proyectos y Servicios NetSuite
-                });
-                objRecord.setValue({
-                    fieldId: "payment",
-                    value: json[3]
-                });
-                var idRecordPago = objRecord.save({
-                    enableSourcing: false,
-                    ignoreMandatoryFields: false
-                });
-                payments.push(idRecordPago);
+                try{
+                    var objRecord = record.create({
+                        type: record.Type.CUSTOMER_PAYMENT,
+                        isDynamic: true
+                    });
+                    var name = json[0];
+                    log.debug("name", name);
+                    var idCustomer = nominas.searchCustomer(name);
+                    log.debug("idCustomer", idCustomer);
+                    objRecord.setValue({
+                        fieldId: "customer",
+                        value: idCustomer // 42866
+                    });
+                    objRecord.setValue({
+                        fieldId: "trandate",
+                        value: format.parse({ value: '12/08/2022', type: format.Type.DATE })
+                    });
+                    objRecord.setValue({
+                        fieldId: "subsidiary",
+                        value: 5 // 5 -> pruebas desarrollo || 20 -> Proyectos y Servicios NetSuite
+                    });
+                    objRecord.setValue({
+                        fieldId: "payment",
+                        value: json[3]
+                    });
+                    var idRecordPago = objRecord.save({
+                        enableSourcing: false,
+                        ignoreMandatoryFields: false
+                    });
+                    payments.push(idRecordPago);
+                } catch(e){
+                    log.debug("Error en registro de pagos", e.message); 
+                    return [0,e.message];
+                }
             });
             return payments;
         }
+
+        function updateState(idPayroll, state){
+            var objRecord  = record.load({
+                type: "customrecord_2win_regist_nominas_de_pago",
+                id: idPayroll
+            });
+            objRecord.setValue({
+                fieldId: 'custrecord_state',
+                value: state,
+                ignoreFieldChange: true
+            });
+            objRecord.save({
+                enableSourcing: false,
+                ignoreMandatoryFields: false
+            });
+            
+        }
         return {
             readPayrollFile : readPayrollFile,
-            registerPayroll : registerPayroll
+            registerPayroll : registerPayroll,
+            updateState : updateState
         };
     
 });
