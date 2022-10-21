3a857731a29be4dabb0c5eabdb5dcd03bdf64bed
DimmoX <86175879+DimmoX@users.noreply.github.com>
2022-10-21 00:26:13 -0300

    add parámetros de conexión, nombres archivos y proceso registro descarga

--- 2win_ss_descarga_archivo_nomina.js
+++ 2win_ss_descarga_archivo_nomina.js
@@ -2,47 +2,88 @@
  *@NApiVersion 2.x
  *@NScriptType ScheduledScript
  */
-define(['./libs/2WinConexionSftp', './libs/2WinUtilityStaticParams'], 
+define(['./libs/2WinConexionSftp', './libs/2WinStaticParamsFacturacion', 'N/record', 'N/format', 'N/runtime'], 
 
-function(sftp, staticParams) {
+function(sftp, params, record, format, runtime) {
 
     var paramConnect = {
-        username: staticParams.getParam('SFTP - username').text,
-        passwordGuid: 'bfe8349be9754672876a432eea4a38be', //passwordGuid: staticParams.getParam('SFTP - passwordGuid').text,
-        url: staticParams.getParam('SFTP - url').text,
-        directory: staticParams.getParam('SFTP - directory Root').text,
-        port: Number(staticParams.getParam('SFTP - port').number),
-        hostKey: staticParams.getParam('SFTP - hostKey').text,
-        hostKeyType: staticParams.getParam('SFTP - hostKeyType').text
+        username: params.getParam('sftp_origen_username').text,
+        passwordGuid: params.getParam('sftp_origen_passwordguid').text, 
+        url: params.getParam('sftp_origen_url').text,
+        directory: params.getParam('sftp_origen_directoryroot').text,
+        port: Number(params.getParam('sftp_origen_port').number),
+        hostKey: params.getParam('sftp_origen_hostkey').text,
+        hostKeyType: params.getParam('sftp_origen_hostkeytype').text
     }
 
     function execute(context) {
+
+        var fileNameDownload = "";
         
         try {
 
             log.debug("Descarga Archivo Nómina", "**** INICIA DESCARGA ****");
 
+            // Crear conexión a servidor SFTP
             var connectSftp  = sftp.setConnection(paramConnect);
+            var fecha = new Date();
+            // pago_pat_pac.csv
+            var filesName = [
+                "patpac_" + fecha.getFullYear() + (fecha.getMonth() + 1) + fecha.getDate() + ".csv",
+                "servipag_" + fecha.getFullYear() + (fecha.getMonth() + 1) + fecha.getDate() + ".txt",
+                "cajavecina_" + fecha.getFullYear() + (fecha.getMonth() + 1) + fecha.getDate() + ".txt"
+            ]
 
-            var folderNameDownload = "/resultados";
-            var fileNameDownload = "pago_pat_pac.csv";            
-            log.debug("Descarga Archivo Nómina - Archivo a Descargar", folderNameDownload + "/" + fileNameDownload);
+            // Iterar los archivos que se deben descargar
+            filesName.forEach(function (fileName) {
 
-            var folderNameTarget = "archivo_nomina";
-            log.debug("Descarga Archivo Nómina - Carpeta Destino", folderNameTarget);
+                fileNameDownload = fileName;
 
-            var fileId = sftp.dowloadFileSftp(fileNameDownload, folderNameDownload, folderNameTarget, connectSftp);
+                var folderNameDownload = "/resultados";     
+                log.debug("Descarga Archivo Nómina - Archivo a Descargar", folderNameDownload + "/" + fileNameDownload);
 
-            log.debug("Descarga Archivo Nómina - Id Archivo Registrado", fileId);
+                var folderNameTarget = "archivo_nomina";
+                log.debug("Descarga Archivo Nómina - Carpeta Destino", folderNameTarget);
+
+                try {
+
+                    var fileId = sftp.dowloadFileSftp(fileNameDownload, folderNameDownload, folderNameTarget, connectSftp);
+
+                    log.debug("Descarga Archivo Nómina - Id Archivo Registrado", fileId);
+
+                    saveProcess(fileNameDownload, "Descargado", "Archivo id " + fileId + " descargado correctamente");
+                    
+                } catch (error) {
+                    if (error.name == "FTP_NO_SUCH_FILE_OR_DIRECTORY") {
+                        log.error("Descarga Archivo Nómina - No Encontrado", error.message);
+                        saveProcess(fileNameDownload, "No Encontrado", error.message);
+                    } else {                        
+                        throw error;
+                    }                    
+                }                
+                
+            });
 
             log.debug("Descarga Archivo Nómina", "**** FIN DESCARGA ****");
             
         } catch (error) {
             log.error("Descarga Archivo Nómina - Error", error);
+            saveProcess(fileNameDownload, "Error de Conexión", error.message);      
         }
     }
 
     return {
         execute: execute
+    }
+    
+    function saveProcess(file, state, message) {
+        
+        var nominaRecord = record.create({ type: 'customrecord_2win_regist_nominas_de_pago', isDynamic: true });
+        nominaRecord.setValue({ fieldId: 'custrecord_name_file', value: file });
+        nominaRecord.setValue({ fieldId: 'custrecord_date_time', value: format.parse({ value: new Date(), type: format.Type.DATE }) });
+        nominaRecord.setValue({ fieldId: 'custrecord_state', value: state });
+        nominaRecord.setValue({ fieldId: 'custrecord_user', value: runtime.getCurrentUser().name });
+        nominaRecord.setValue({ fieldId: 'custrecord_2win_observacion', value: message });
+        nominaRecord.save();
     }
 });
